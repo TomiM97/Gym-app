0e6e4dc1dbfcab1529a9989bc37b9e710c0e756b
TomiM97 <tomi.maijala@student.lut.fi>
2023-04-24 22:36:32 +0300

    main acticity fragmentit

--- app/src/main/java/com/example/gymapp/MainActivity.java
+++ app/src/main/java/com/example/gymapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.gymapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tabArea);
+        ViewPager2 fragmentArea = findViewById(R.id.fragmentArea);
+        TabPagerAdapter tabPagerAdapter = new TabPagerAdapter(this);
+        fragmentArea.setAdapter(tabPagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                fragmentArea.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        fragmentArea.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.getTabAt(position).select();
+            }
+        });
     }
 
     public void kakkaaksdee(){
