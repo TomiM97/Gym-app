289a9297419383660a7fd30cce77166ecdd9271a
Vili05 <104265232+Vili05@users.noreply.github.com>
2023-04-25 20:31:04 +0300

    weight kuvaajaa tehty

--- app/src/main/java/com/example/gymapp/fragments/FragmentWeight.java
+++ app/src/main/java/com/example/gymapp/fragments/FragmentWeight.java
@@ -4,56 +4,28 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.gymapp.R;
+import com.example.gymapp.Weight;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentWeight#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class FragmentWeight extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FragmentWeight() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentWeight.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentWeight newInstance(String param1, String param2) {
-        FragmentWeight fragment = new FragmentWeight();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    GraphView graphView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
@@ -61,6 +33,32 @@ public class FragmentWeight extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weight, container, false);
+        View view = inflater.inflate(R.layout.fragment_weight, container, false);
+        EditText editWeight = view.findViewById(R.id.editWeight);
+        EditText editDate = view.findViewById(R.id.editDate);
+        Button btnAddWeight = view.findViewById(R.id.btnAddWeight);
+        GraphView weightGraph = view.findViewById(R.id.weightGraph);
+
+        btnAddWeight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Float inputWeight = Float.parseFloat(editWeight.getText().toString());
+                Date date;
+                try {
+                    date = new SimpleDateFormat("dd.MM.yyyy").parse(editDate.getText().toString());
+                } catch (ParseException e) {
+                    throw new RuntimeException(e);
+                }
+                Weight weight = new Weight(inputWeight, date);
+                Weight.getInstance().addWeight(weight);
+
+                editWeight.setText("");
+                editDate.setText("");
+            }
+        });
+
+
+
+        return view;
     }
 }
