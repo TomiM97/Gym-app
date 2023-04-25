9f1c320a1d0c82fac7d14c14e7cf4043e9f0455e
Tuomashui <tuomashui@gmail.com>
2023-04-25 18:00:57 +0300

    toni toni toni viikinki halmeeeee

--- app/src/main/java/com/example/gymapp/TabPagerAdapter.java
+++ app/src/main/java/com/example/gymapp/TabPagerAdapter.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
-
 import com.example.gymapp.fragments.FragmentFrontPage;
 import com.example.gymapp.fragments.FragmentGraphs;
 import com.example.gymapp.fragments.FragmentWeight;
