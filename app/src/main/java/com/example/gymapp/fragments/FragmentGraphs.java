9c86f00b54a94d1cb6a15cc8a92bcdb0652557da
Tuomashui <tuomashui@gmail.com>
2023-04-27 17:57:27 +0300

    FragmentGraphs päivitetty ja mahdollisesti hajotettu

--- app/src/main/java/com/example/gymapp/fragments/FragmentGraphs.java
+++ app/src/main/java/com/example/gymapp/fragments/FragmentGraphs.java
@@ -2,16 +2,27 @@ package com.example.gymapp.fragments;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 
+import com.example.gymapp.Exercise;
 import com.example.gymapp.R;
+import com.example.gymapp.Workout;
+import com.example.gymapp.rv_holders_and_adapters.NewWorkoutRVAdapter;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -32,7 +43,6 @@ public class FragmentGraphs extends Fragment {
     public FragmentGraphs() {
         // Required empty public constructor
     }
-
 
     public static FragmentGraphs newInstance(String param1, String param2) {
         FragmentGraphs fragment = new FragmentGraphs();
@@ -64,8 +74,48 @@ public class FragmentGraphs extends Fragment {
             series.appendData(new DataPoint(x,y), true,90);
         }
         graphExercise.addSeries(series);
+        Button addExercise = view.findViewById(R.id.btnshowExercise);
+        // RecyclerView needs start:
+        RecyclerView recyclerView = view.findViewById(R.id.rvExerciselist);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        NewWorkoutRVAdapter newWorkoutRVAdapter = new NewWorkoutRVAdapter(getContext(), Workout.getInstance().getExercises());
+        recyclerView.setAdapter(newWorkoutRVAdapter);
+        // RecyclerView needs stop.
+        addExercise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View addExercisePopupView = getLayoutInflater().inflate(R.layout.popup_add_exercise, null);
 
+                exercise = addExercisePopupView.findViewById(R.id.txtExerciseName);
+                // Dropdown menu
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_dropdown_item_1line,
+                        Workout.getInstance().getExerciseNames(Workout.getInstance().getExercises()));
 
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(view.getContext());
+                Button popupShowExercise = addExercisePopupView.findViewById(R.id.btnshowExercise);
+                Button popupCancel = addExercisePopupView.findViewById(R.id.btnCancelPopup);
+                dialogBuilder.setView(addExercisePopupView);
+                final AlertDialog dialog = dialogBuilder.create();
+                dialog.show();
+                popupShowExercise.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        ArrayList<Integer> repsList = new ArrayList<>();
+                        ArrayList<Float> weightsList = new ArrayList<>();
+
+                        String txtNewExercise = newExercise.getText().toString();
+
+                        Workout.getInstance().addExercise(new Exercise(weightsList, sets, repsList, txtNewExercise));
+                        newWorkoutRVAdapter.notifyDataSetChanged();
+                        dialog.dismiss();}});
+                popupCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
         return view;
     }
 }
